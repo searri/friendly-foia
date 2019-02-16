91be5869f47d5917bfde539021f60e6380264f2c
Tfkalk <33587880+Tfkalk@users.noreply.github.com>
2019-02-16 16:29:08 -0500

    Updated with method

--- Form Getter.py
+++ Form Getter.py
@@ -1,7 +1,6 @@
-import requests
+def doForm(id):
+	headers = {
+		'X-API-Key': 'zAI65kxpNF2gJQfEpd6vP1ughBhwFu2UCZNVHwht',
+	}
 
-headers = {
-    'X-API-Key': 'zAI65kxpNF2gJQfEpd6vP1ughBhwFu2UCZNVHwht',
-}
-
-response = requests.get('https://api.foia.gov/api/agency_components/[[ID]]/request_form', headers=headers)
+	response = requests.get('https://api.foia.gov/api/agency_components/' + id '/request_form', headers=headers)
