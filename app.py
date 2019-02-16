682c70d8bca1a8e42f771c1a4d619dc7fc1146fa
Rick Sear <yobob.sear@gmail.com>
2019-02-16 14:37:02 -0500

    Agency selection list working

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template('landingpage.html')
+
+@app.route('/findagency', methods=['POST'])
+def findagency():
+    agency = request.form['agency']
+    print(agency)
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
