dd58481bc68edb5ff053ae93695a4d414c3498a4
Rick <31972051+searri@users.noreply.github.com>
2019-02-19 10:15:59 -0500

    Important update

--- FormGetter.py
+++ FormGetter.py
@@ -3,7 +3,7 @@ import json
 
 def doForm(id):
 	headers = {
-		'X-API-Key': 'zAI65kxpNF2gJQfEpd6vP1ughBhwFu2UCZNVHwht',
+		'X-API-Key': 'MY_KEY',
 	}
 
 	response = requests.get('https://api.foia.gov/api/agency_components/8216158f-8089-431d-b866-dc334e8d4758/request_form' + id + '/request_form', headers=headers)
